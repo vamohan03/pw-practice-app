dd7ab2789c5a2c5d587208b1bb17821af008dc3d
Vamshi Bonala <vamohan.877@gmail.com>
2024-07-31 16:41:31 -0700

    locator syntax rules added

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -3,17 +3,41 @@ import { test } from "@playwright/test"
 test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200')
     await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
 
 })
 
-test ('the first test' , async ({page}) => {
+test ('locator syntax rules' , async ({page}) => {
 
-    await page.getByText('Form Layouts').click()
+    //by tag name
+    await page.locator('input').click()
+
+    //by id
+    await page.locator('#inputEmail1').click()
+
+    //by class
+    page.locator('.shape-rectangle')
+
+    //by attribute
+    page.locator('[placeholder="Email"]')
+
+    //by class value (full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    //combine different locators
+    page.locator('input[placeholder="Email"]')
+
+    //by Xpath
+    page.locator('//*[@id="inputEmail1"]')
+
+    //by text partial
+    page.locator(':text("Using")')
+
+    //by exact match text
+    page.locator(':text-is("Using the grid")')
+
+
+
 
 }) 
 
-test ('Navigate to datepicker' , async ({page}) => {
-
-    await page.getByText('Datepicker').click()
-
-}) 
